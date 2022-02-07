43e28b7e95a4650aa997dc98aa0fbe4db9e9a81a
Roberto Rodriguez <roberto@rodriguezartavia.com>
2022-02-07 08:09:38 -0600

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { DefaultSeo } from "next-seo";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+  heap.load("1604821807")`,
+        }}
+      />
+
       <DefaultSeo
         title="Paz | Cabo Mata Palo | Corcovado | Costa Rica"
         description="Private Beach Lounge | Comfy Jungle House | Natural Wellness"
