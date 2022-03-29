17ca8877dd60ba8aa4900b6f70bdaf13d0813eb8
roberto <roberto@robertos-MacBook-Air.local>
2022-03-28 20:38:28 -0600

    update

--- components/marketing/hero.js
+++ components/marketing/hero.js
@@ -73,6 +73,30 @@ export default function Hero(props) {
         </main>
 
         <main className="relative    sm:hidden">
+          <div className=" absolute bottom-0 w-full  sm:col-span-3 pt-2 border-l-2 border-white  bg-gradient-to-br from-violet-400 to-violet-500 opacity-80 lg:pt-10 ">
+            <div className=" sm:max-w-3xl   ">
+              <div className="relative">
+                <div className=" sm:max-w-xl pl-4 ">
+                  <div className="flex">
+                    <img
+                      className="h-20 w-20"
+                      src="/marketing/logo/svg/paz_blanco.svg"
+                    />
+                    <div className="pt-4">
+                      <p className=" text-2xl text-white shadow-sm ">
+                        Meaningful Vacations
+                      </p>
+                      <p className=" text-sm uppercase text-violet-800  ">
+                        Transformational Retreats
+                      </p>
+                    </div>
+                  </div>
+
+                  <PlayIcon />
+                </div>
+              </div>
+            </div>
+          </div>
           <video
             poster="/images/video-bkg.jpg"
             ref={video1Ref}
@@ -80,7 +104,6 @@ export default function Hero(props) {
             className="col-span-1 sm:col-span-4  border-l-2 border-white   w-2xl  "
             autoPlay
             muted
-            controls
           >
             <source src="./videos/result_2.mp4" type="video/mp4" />
           </video>
