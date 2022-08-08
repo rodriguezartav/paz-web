27206baed4171b42e56fe9297f66fdcb012084c3
roberto <roberto@robertos-MacBook-Air.local>
2022-08-07 21:52:31 -0600

    udate links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -63,9 +63,15 @@ export function Header() {
               </span>
             </Link>
 
-            {/* <GuidesPopover /> */}
+            <GuidesPopover />
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+            <a
+              href="/podcast"
+              className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent border-indigo-600 px-4 py-2 text-base font-medium text-indigo-600 shadow-sm hover:bg-indigo-700 hover:text-white"
+            >
+              Podcast
+            </a>
             <a
               href="/reservation"
               className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
